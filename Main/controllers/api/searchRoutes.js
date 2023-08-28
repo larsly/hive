dfb6471f85e38dfabd5e24644ca5cec30305d572
larsly <larissa.bierly@gmail.com>
2023-08-28 19:04:50 -0400

    started searchRoute.js

---
+++ Main/controllers/api/searchRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Search } = require('../../models');
+
+// Define a route to search the database
+router.get('/api/search', async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const searchEvent = await Event.findByPk(req.session.user_id, {
+          attributes: { exclude: ['password'] },
+          include: [{ model: Event }],
+        });
+    
+        const user = searchEvent.get({ plain: true });
+    
+        res.render('profile', {
+          ...user,
+          logged_in: true
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+});
+
+module.exports = router;
