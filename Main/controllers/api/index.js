ddb4683773dd0f0fd4480f3809825f936e90e10d
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-08-28 20:43:56 -0400

    adds new profile view and profile.js to handle profile setup

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const eventRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 
-router.use('/users', eventRoutes);
-router.use('/projects', projectRoutes);
+router.use('/users', userRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
