b6af1d6af0531dbdf1967f9d7e23bbb6b4954469
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-08-29 21:35:49 -0400

    updates server.js and homeRoutes and adds a comment model

--- Main/models/User.js
+++ Main/models/User.js
@@ -16,17 +16,11 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    user_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+ 
     },
     password: {
       type: DataTypes.STRING,
@@ -35,7 +29,6 @@ User.init(
         len: [8],
       },
     },
-  },
   {
     hooks: {
       beforeCreate: async (newUserData) => {
--- Main/server.js
+++ Main/server.js
@@ -1,43 +1,39 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({
+    helpers
+});
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  cookie: {},
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: new SequelizeStore({
     db: sequelize
   })
 };
 
-app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
 
 app.use(routes);
 
